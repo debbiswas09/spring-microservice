bec068d62319ebc9e7dda66340d34669dadf5bd0
debbiswas09 <debbiswas08031997@gmail.com>
2023-04-22 11:53:21 +0530

    implementing distributed tracing
    Added micrometer, opentelemetry & zipkin

--- microservices/currency-exchange-service/src/main/java/com/deb/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ microservices/currency-exchange-service/src/main/java/com/deb/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class CircuitBreakerController {
